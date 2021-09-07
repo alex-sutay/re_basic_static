9250d9a07311bfcea6e896d792152153bfc12bc8
alex-sutay <ads1429@rit.edu>
2021-09-07 19:06:09 -0400

    Added imports and actual docstrings

--- re_basic_static.py
+++ re_basic_static.py
@@ -1,35 +1,62 @@
 # file: re_basic_static.py
 # author: Alex Sutay
 
+# built-in imports
+import re  # used to extract strings from exe
+import argparse  # used to parse the commandline arguments
+# pip libraries
+import pefile  # used to read the pe headers of exe files
 
-# find all of the strings
+
 def extract_strings(exe_file_name):
-    pass
+    """
+    find all of the strings within an executable file
+    :param exe_file_name: string path to the executable
+    :return: lst of strings
+    """
 
 
-# find all of the imports
 def extract_imports(exe_file_name):
-    pass
+    """
+    find all of the imports within an executable file
+    :param exe_file_name: string path to the executable
+    :return: lst of string names of imports
+    """
 
 
-# find compiler and compile time
 def extract_compile_info(exe_file_name):
-    pass
+    """
+    find the compiler and compile time from an executable
+    :param exe_file_name: string path to the executable
+    :return: dict of compiler information
+    """
 
 
-# given a list of strings, find IPs, URLs, file paths, registry keys
 def eval_strings(exe_strings):
-    pass
+    """
+    given a list of strings, find the strings that are of particular interest
+    currently looks for IPs, URLs, file paths, and registry keys TODO
+    return is in the form {'label': ['string_1', string_2', ...], ...}
+    :param exe_strings: lst of strings
+    :return: dict of lst of interesting strings
+    """
 
 
-# given a list of imports, filter for typical malware imports
 def eval_imports(exe_imports):
-    pass
+    """
+    given a list of imports, filter for typical malware imports
+    return is in the form {'label': ['import_1', 'import_2', ...], ...}
+    :param exe_imports: lst of string imports
+    :return: dict of lst of string imports
+    """
 
 
-# Run all of the functions
 def main():
-    pass
+    """
+    The primary function that is called
+    Evaluates the commandline arguments, runs the appropriate functions, and prints the output
+    :return: None
+    """
 
 
 if __name__ == '__main__':
