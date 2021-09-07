1689619fd6bc89cdce31cfcf56f2981f3d3344ed
alex-sutay <ads1429@rit.edu>
2021-09-07 19:41:21 -0400

    It'll find your strings now

--- re_basic_static.py
+++ re_basic_static.py
@@ -8,12 +8,18 @@ import argparse  # used to parse the commandline arguments
 import pefile  # used to read the pe headers of exe files
 
 
-def extract_strings(exe_file_name):
+def extract_strings(exe_file_name, min_len=4):
     """
     find all of the strings within an executable file
     :param exe_file_name: string path to the executable
+    :param min_len: the minimum length of string to find; defaults to 4
     :return: lst of strings
     """
+    with open(exe_file_name, 'rb') as f:
+        exe_data = f.read()
+    pattern = b"[\x1f-\x7e]{" + str(min_len).encode() + b",}"
+    byte_strings = re.findall(pattern, exe_data)
+    return sorted(a.decode() for a in byte_strings)
 
 
 def extract_imports(exe_file_name):
@@ -57,6 +63,12 @@ def main():
     Evaluates the commandline arguments, runs the appropriate functions, and prints the output
     :return: None
     """
+    parser = argparse.ArgumentParser(description='A tool to simplify basic static analysis')
+    parser.add_argument('exe_file_name', metavar='exe_file', type=str,
+                        help='The name of the exe file being evaluated')
+    parser.add_argument('--string-length', help='The minimum length of string to check for the strings section')
+    args = parser.parse_args()
+    print(extract_strings(args.exe_file_name, args.string_length) if args.string_length else extract_strings(args.exe_file_name))
 
 
 if __name__ == '__main__':
