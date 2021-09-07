3fd2038381386d305394980dc4e0e9d80fe462ef
alex-sutay <ads1429@rit.edu>
2021-09-07 18:50:11 -0400

    Initial commit

---
+++ re_basic_static.py
@@ -0,0 +1,36 @@
+# file: re_basic_static.py
+# author: Alex Sutay
+
+
+# find all of the strings
+def extract_strings(exe_file_name):
+    pass
+
+
+# find all of the imports
+def extract_imports(exe_file_name):
+    pass
+
+
+# find compiler and compile time
+def extract_compile_info(exe_file_name):
+    pass
+
+
+# given a list of strings, find IPs, URLs, file paths, registry keys
+def eval_strings(exe_strings):
+    pass
+
+
+# given a list of imports, filter for typical malware imports
+def eval_imports(exe_imports):
+    pass
+
+
+# Run all of the functions
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
